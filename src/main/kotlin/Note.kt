14a4137ffe4ee79c37c1f1d7704a6fe5118c98e4
Gorkaisidro <gorkaisidro19@gmail.com>
2024-04-25 19:30:32 +0200

    Crear la función getNotes() que devuelva mediante un rango del 1 al 10 el listado de notas

--- src/main/kotlin/Note.kt
+++ src/main/kotlin/Note.kt
@@ -1,19 +1,13 @@
-
+import Note.Type
 
 data class Note (val title: String, val description: String, val type: Type) {
     enum class Type { TEXTO, AUDIO }
 }
 
-val list: List<Note> = listOf(
-    Note("Title 1", "Description 1", Note.Type.AUDIO),
-    Note("Title 2", "Description 2", Note.Type.AUDIO),
-    Note("Title 3", "Description 3", Note.Type.TEXTO),
-    Note("Title 4", "Description 4", Note.Type.TEXTO),
-    Note("Title 5", "Description 5", Note.Type.AUDIO),
-    Note("Title 6", "Description 6", Note.Type.AUDIO),
-    Note("Title 7", "Description 7", Note.Type.TEXTO),
-    Note("Title 8", "Description 8", Note.Type.TEXTO),
-    Note("Title 9", "Description 9", Note.Type.AUDIO),
-    Note("Title 10", "Description 10", Note.Type.TEXTO),
-)
-
+fun getNotes(): List<Note> = (1 .. 10).map {
+    Note(
+        "Title $it",
+        "Description $it",
+        if(it % 3 == 0) Type.AUDIO else Type.TEXTO
+    )
+}
